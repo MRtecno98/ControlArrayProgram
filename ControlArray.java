95d452eb8588157724391a8b62b1ef04ac2f3ab4
BuildTools <unconfigured@null.spigotmc.org>
2016-07-08 20:03:47 +0200

    inizio secondo branch + risolti problemi

--- ControlArray.java
+++ ControlArray.java
@@ -2,37 +2,33 @@ public class ControlArray
 {
 	public static void main(String[]args)
 	{
-		int AR1[] = {4732,3712,4732,4572,98};
 		
-		public static ControlArray(String NomeAR)
-		{
+		int AR1[] = {4732,3712,4732,4572,98};
+        
+		
 			boolean Positivo = false;
 			
-			System.out.println("Inizio controllo array: " + NomeAR);
+			System.out.println("Inizio controllo array: AR1");
 			
-			for(int i = 0, i <= NomeAR.lenght, i++)
+			for (int i = 0; i < 4; i++)
 			{
-				System.out.println("Slot " + i + " = " + NomeAR[i]);
+				System.out.println("Slot " + i + " = " + AR1[i]);
 				
-				if (NomeAR[i] = 98)
+				if (AR1[i] == 98)
 				{
 					Positivo = true;
 				}
 				
 			}
 			
-			if (Positivo = true)
+			if (Positivo == true)
 			{
-				System.out.println("L'Array " + NomeAR + " è risultato positivo");	
+				System.out.println("L'Array AR1 è risultato positivo");	
 			}
-			 else
-			 {
-				System.out.println("L'Array " + NomeAR + " è risultato negativo");	
-			 }
+			else
+			{
+				System.out.println("L'Array AR1 è risultato negativo");	
+			}
 			
-			
-		}
-		
-		
 	}
 }
